2543dcd774d2b7807a8efe891d66d47f3eaa2c60
Drowsy <n.kaneta.1026@gmail.com>
2023-04-27 10:59:46 +0900

    ローデイング画面の作成

---
+++ components/Loading.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="text">
+    <p v-if="isShow" class="loading">
+      {{ randomString }}<br />
+      {{ randomString }}<br />
+      {{ randomString }}<span>---LOADING...---</span>{{ randomString }}<br />
+      {{ randomString }}<br />
+      {{ randomString }}
+    </p>
+    <div class="shful-div">
+      <p v-if="randomShow" class="shful">{{ randomString }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  component: {},
+  data() {
+    return {
+      isShow: false,
+      randomShow: true,
+      randomString: "",
+      randomString2: "",
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.randomShow = false;
+      this.isShow = true;
+    }, 1000);
+
+    setInterval(() => {
+      this.randomString = this.generateRandomString();
+      this.randomString2 = this.generateRandomString2();
+    }, 80);
+  },
+  methods: {
+    generateRandomString() {
+      let length = 100;
+      let result = "";
+      let characters = "-01";
+      let charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
+    },
+    generateRandomString2() {
+      let length = 40;
+      let result = "";
+      let characters = "-01";
+      let charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.text {
+  background-color: black;
+  width: 100%;
+  height: 100vh;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  position: fixed;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.loading {
+  font-size: 30px;
+  color: #00ff00;
+}
+
+.shful {
+  font-size: 30px;
+  color: #00ff00;
+}
+
+.shful-div {
+  white-space: nowrap;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <Loading v-if="isShow" />
+    <!-- <h2>{{ randomString }}</h2> -->
+  </div>
+  <main>
+    <nav>ここにナビゲーションバーを入れる</nav>
+    <slot />
+  </main>
+</template>
+
+<script>
+import Loading from "~/components/Loading.vue";
+
+export default {
+  component: {
+    Loading,
+  },
+  data() {
+    return {
+      isShow: true,
+      randomString: "",
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.isShow = false;
+      }, 2000);
+    });
+    setInterval(() => {
+      this.randomString = this.generateRandomString();
+    }, 50);
+  },
+  methods: {
+    generateRandomString() {
+      let length = 100;
+      let result = "";
+      let characters = "-01/¥|@+=?!.,:;";
+      let charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sh-text {
+  color: black;
+}
+</style>
